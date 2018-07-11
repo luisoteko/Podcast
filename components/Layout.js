35d2cbcd5876a72de7f47152650eb00afd67628c
Luisoteko <luisoteko100@gmail.com>
2018-07-11 17:08:32 -0500

    Created React Components, added ViewPort

---
+++ components/Layout.js
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import Head from 'next/head'
+export default class Layout extends React.Component {
+  render() {
+    const { children, title } = this.props
+    return (
+      <div>
+        <Head>
+          <title>{ title }</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        </Head>
+        <header><Link href="/"><a >Podcast</a></Link></header>
+        { children }
+        <style jsx>{`
+          header{
+              color: #fff;
+              background: #8756ca;
+              padding: 15px;
+              text-align: center;
+          }
+          header a{
+              color: #fff;
+              text-decoration: none
+          }
+        `}</style>
+        <style jsx global>{`
+          body{
+              margin: 0;
+              background: white;
+              font-family: system-ui;
+          }                                    
+        `}</style>
+      </div>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,21 @@
-import 'isomorphic-fetch'
-import Link from 'next/link'
+import 'isomorphic-fetch';
+import Layout from '../components/Layout'
+import ChannelGrid from '../components/ChannelGrid'
 
 export default class extends React.Component {
   
   static async getInitialProps(){
     let req = await fetch('https://api.audioboom.com/channels/recommended')
     let {body: channels} = await req.json()
-    
     return { channels }
   }
 
   render() {
     const { channels } = this.props
     return (
-      <div>
-        <header>Podcast</header>
-        <div className="channels">
-          { channels.map((channel)=>(
-              <Link href={`/channel?id=${ channel.id }`} prefetch>
-                <a className="channel">
-                  <img src={channel.urls.logo_image.original} alt={channel.title}/>
-                  <h2>{channel.title}</h2>
-                </a>
-              </Link>
-            ))
-          }
-        </div>
-        <style jsx>{`
-          header{
-            color: #fff;
-            background: #8756ca;
-            padding: 15px;
-            text-align: center;
-          }
-          .channels{
-            display: grid;
-            grid-gap: 15px;
-            padding: 15px;
-            grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
-          }
-          .channel{
-            display: block;
-            border-radius: 3px;
-            box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
-            margin-bottom: 0,5em
-          }
-          .channel img{
-            width: 100%
-          }
-          h2{
-            padding: 5px;
-            font-size: 0.9 em;
-            font-weight: 600;
-            margin: 0;
-            text-align: center;
-          }
-        `}</style>
-        <style jsx global>{`
-          body{
-            margin: 0;
-            background: white;
-            font-family: system-ui;
-          }
-        `}</style>
-      </div>
+      <Layout title="Podcast">
+        <ChannelGrid channels={channels}/>
+      </Layout>
     )
   }
 }
