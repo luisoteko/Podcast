7b9855cbbd4cbbc09fa75a008f11adcd162a1546
Luisoteko <luisoteko100@gmail.com>
2018-07-11 11:14:19 -0500

    Showing Podcasts, Series and Audios

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,68 @@
+import 'isomorphic-fetch'
+import Link from 'next/link'
+
 export default class extends React.Component {
+  
+  static async getInitialProps(){
+    let req = await fetch('https://api.audioboom.com/channels/recommended')
+    let {body: channels} = await req.json()
+    
+    return { channels }
+  }
+
   render() {
+    const { channels } = this.props
     return (
       <div>
-        <h1>Hola Platzi!</h1>
-        <p>Bienvenidos al curso de Next!</p>
-        <img src="/static/platzi-logo.png" alt="Platzi"/>
-            <style jsx>{`
-                h1{
-                    color: red;
-                }
-                p{
-                  color: green;
-                }
-                :global (div p){
-                  color: green;
-                }
-                img{
-                  max-width: 50%;
-                  display: block;
-                  margin: 0 auto;
-                }
-            `}
-        </style>
+        <header>Podcast</header>
+        <div className="channels">
+          { channels.map((channel)=>(
+              <Link href={`/channel?id=${ channel.id }`} prefetch>
+                <a className="channel">
+                  <img src={channel.urls.logo_image.original} alt={channel.title}/>
+                  <h2>{channel.title}</h2>
+                </a>
+              </Link>
+            ))
+          }
+        </div>
+        <style jsx>{`
+          header{
+            color: #fff;
+            background: #8756ca;
+            padding: 15px;
+            text-align: center;
+          }
+          .channels{
+            display: grid;
+            grid-gap: 15px;
+            padding: 15px;
+            grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+          }
+          .channel{
+            display: block;
+            border-radius: 3px;
+            box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+            margin-bottom: 0,5em
+          }
+          .channel img{
+            width: 100%
+          }
+          h2{
+            padding: 5px;
+            font-size: 0.9 em;
+            font-weight: 600;
+            margin: 0;
+            text-align: center;
+          }
+        `}</style>
+        <style jsx global>{`
+          body{
+            margin: 0;
+            background: white;
+            font-family: system-ui;
+          }
+        `}</style>
       </div>
     )
   }
