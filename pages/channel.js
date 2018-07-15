2f4c3ddcdc3f4c36f0c6451d55ec760d7a0c98a1
Luisoteko <luisoteko100@gmail.com>
2018-07-15 17:57:06 -0500

    Complete App

--- pages/channel.js
+++ pages/channel.js
@@ -1,68 +1,123 @@
-import Layout from '../components/Layout';
-import ChannelGrid from '../components/ChannelGrid';
+import 'isomorphic-fetch'
+import Layout from '../components/Layout'
+import ChannelGrid from '../components/ChannelGrid'
 import PodcastList from '../components/PodcastList'
+import PodcastListWithClick from '../components/PodcastListWithClick'
+import PodcastPlayer from '../components/PodcastPlayer'
+import Error from './_error'
+
 
 export default class extends React.Component {
 
-  static async getInitialProps({ query }) {
+  state = { openPodcast: null }
+
+  static async getInitialProps({ query, res }) {
     let idChannel = query.id
 
-    let [reqChannel, reqSeries, reqAudios] = await Promise.all([
-      fetch(`https://api.audioboom.com/channels/${idChannel}`),
-      fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`),
-      fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`)
-    ])
+    try {
+      let [reqChannel, reqSeries, reqAudios] = await Promise.all([
+        fetch(`https://api.audioboom.com/channels/${idChannel}`),
+        fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`),
+        fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`)
+      ])
 
-    let dataChannel = await reqChannel.json()
-    let channel = dataChannel.body.channel
+      if (reqChannel.status >= 400) {
+        res.statusCode = reqChannel.status
+        return { channel: null, audioClips: null, series: null, statusCode: reqChannel.status }
+      }
 
-    let dataAudios = await reqAudios.json()
-    let audioClips = dataAudios.body.audio_clips
+      let dataChannel = await reqChannel.json()
+      let channel = dataChannel.body.channel
 
-    let dataSeries = await reqSeries.json()
-    let series = dataSeries.body.channels
+      let dataAudios = await reqAudios.json()
+      let audioClips = dataAudios.body.audio_clips
 
-    return { channel, audioClips, series }
+      let dataSeries = await reqSeries.json()
+      let series = dataSeries.body.channels
+
+      return { channel, audioClips, series, statusCode: 200 }
+    } catch (e) {
+      return { channel: null, audioClips: null, series: null, statusCode: 503 }
+    }
+  }
+
+  openPodcast = (event, podcast) => {
+    if (event.metaKey || event.ctrlKey || event.shiftKey || (event.nativeEvent && event.nativeEvent.which === 2)) {
+      // Si está haciendo Ctrl+Click o Cmd+Click, dejamos que el click suceda normalmente
+      return
+    }
+
+    event.preventDefault()
+    this.setState({
+      openPodcast: podcast
+    })
+  }
+
+  closePodcast = (event) => {
+    event.preventDefault()
+    this.setState({
+      openPodcast: null
+    })
   }
 
   render() {
-    const { channel, audioClips, series } = this.props
-    return (
-      <Layout title={channel.title}>
-        <div className="banner" style={ { backgroundImage: `url(${channel.urls.banner_image.original})` } }></div>
+    const { channel, audioClips, series, statusCode } = this.props
+    const { openPodcast } = this.state
 
-        <h1>{ channel.title }</h1>
-        
-        { series.length > 0 &&
-          <div>
-            <h2>Series</h2>
-            <ChannelGrid channels={series}/>
-          </div> }
+    if (statusCode !== 200) {
+      return <Error statusCode={ statusCode } />
+    }
 
-        <h2>Ultimos Podcasts</h2>
-        <PodcastList podcasts={audioClips}/>
-        
-        <style jsx>{`
-          .banner {
-            width: 100%;
-            padding-bottom: 25%;
-            background-position: 50% 50%;
-            background-size: cover;
-            background-color: #aaa;
-          }
-          h1 {
-            font-weight: 600;
-            padding: 15px;
-          }
-          h2 {
-            padding: 5px;
-            font-size: 0.9em;
-            font-weight: 600;
-            margin: 0;
-            text-align: center;
-          }
-        `}</style>
-      </Layout>
-    )
+    return <Layout title={ channel.title }>
+
+      { openPodcast &&
+      <div>
+        <PodcastPlayer clip={ openPodcast } onClose={ this.closePodcast } />
+      </div>
+      }
+
+      <div className="banner" style={ { backgroundImage: `url(${channel.urls.banner_image.original})` } } />
+
+      <h1>{ channel.title }</h1>
+
+      { series.length > 0 &&
+        <div>
+          <h2>Series</h2>
+          <ChannelGrid channels={ series } />
+        </div>
+      }
+
+      <h2>Ultimos Podcasts</h2>
+      <PodcastListWithClick podcasts={ audioClips } onClickPodcast={ this.openPodcast } />
+
+      <style jsx>{`
+        .banner {
+          width: 100%;
+          padding-bottom: 25%;
+          background-position: 50% 50%;
+          background-size: cover;
+          background-color: #aaa;
+        }
+        h1 {
+          font-weight: 600;
+          padding: 15px;
+        }
+        h2 {
+          padding: 15px;
+          font-size: 1.2em;
+          font-weight: 600;
+          margin: 0;
+        }
+        .modal{
+          position: fixed;
+          top: 0;
+          background: black;
+          bottom: 0;
+          right: 0;
+          left: 0;
+          z-index: 999999;
+        }
+      `}</style>
+    </Layout>
   }
 }
---
+++ routes.js
@@ -0,0 +1,6 @@
+const routes = require('next-routes')
+
+module.exports = routes()
+  .add('about')
+  .add('channel', '/:slug.:id', 'channel')
+  .add('podcast', '/:slugChannel.:idChannel/:slug.:id', 'podcast')
